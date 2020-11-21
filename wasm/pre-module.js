fee0f700a34d6745366ab673d109b12035d82083
Motin <motin@motin.eu>
2020-11-22 00:24:57 +0200

    Add hardcoded stdin input (untested since another exception prevents testing at this point)

--- wasm/pre-module.js
+++ wasm/pre-module.js
@@ -1,2 +1,40 @@
 // Enables setting runtime args via the query string:
 Module["arguments"] = window.location.search.substr(1).split('%20')
+var initStdInOutErr = function() {
+  var input = "Hola mundo\n";
+  var i = 0;
+  function stdin() {
+    if (i < input.length) {
+      var code = input.charCodeAt(i);
+      ++i;
+      console.log("STDIN: Feeding character code to stdin: ", code);
+      return code;
+    } else {
+      console.log("STDIN: Done feeding input via stdin: ", input);
+      return null;
+    }
+  }
+
+  var stdoutBuffer = "";
+  function stdout(code) {
+    if (code === "\n".charCodeAt(0) && stdoutBuffer !== "") {
+      console.log("STDOUT: ", stdoutBuffer);
+      stdoutBufer = "";
+    } else {
+      stdoutBuffer += String.fromCharCode(code);
+    }
+  }
+
+  var stderrBuffer = "";
+  function stderr(code) {
+    if (code === "\n".charCodeAt(0) && stderrBuffer !== "") {
+      console.log("STDERR: ", stderrBuffer);
+      stderrBuffer = "";
+    } else {
+      stderrBuffer += String.fromCharCode(code);
+    }
+  }
+
+  FS.init(stdin, stdout, stderr);
+}
+Module["preRun"].push(initStdInOutErr);
