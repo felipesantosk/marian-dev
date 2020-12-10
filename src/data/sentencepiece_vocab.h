1096614de26fa8a2fc7bcb6a385f19ad748210d3
Jerin Philip <jphilip@ed.ac.uk>
2020-12-10 16:53:39 +0000

    SentencePieceText: header -> forward-declare
    Changes a generated file header inclusion to a forward-declaration.
    Trouble seems to go away.

--- src/data/sentencepiece_vocab.h
+++ src/data/sentencepiece_vocab.h
@@ -3,18 +3,6 @@
 #ifdef USE_SENTENCEPIECE
 #include "sentencepiece/src/sentencepiece_processor.h"
 #include "sentencepiece/src/sentencepiece_trainer.h"
-
-/* https://github.com/google/googletest/issues/1063#issuecomment-332518392 */
-#if __GNUC__ >= 5
-// Disable GCC 5's -Wsuggest-override warnings in gtest
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wsuggest-override"
-#endif
-#include "sentencepiece/src/builtin_pb/sentencepiece.pb.h"
-#if __GNUC__ >= 5
-#pragma GCC diagnostic pop
-#endif
-
 #endif
 
 #include "common/config.h"
@@ -25,6 +13,12 @@
 
 #include <random>
 #include <sstream>
+
+
+
+namespace sentencepiece {
+  class SentencePieceText;
+}
 
 namespace marian {
 #ifdef USE_SENTENCEPIECE
